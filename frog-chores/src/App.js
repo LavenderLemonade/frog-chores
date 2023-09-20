4c44770cb155b842b7f58a3d0f4d137e433c957f
Sam Elliott <soe1@uakron.edu>
2023-09-20 19:14:24 -0400

    we got the frog in! and it is all centered!

--- frog-chores/src/App.js
+++ frog-chores/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import chores from './choresdone.png'
+import expectantFrog from './firstfrog.png'
 
 function App() {
   return (
     <div className="App">
-
       <img src={chores} alt='choresdone' />
-
+      <img src={expectantFrog} alt='frog1' />
     </div>
   );
 }
