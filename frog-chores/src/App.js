200152ced58fdd225c3b5aa7dc99335036b77c8a
Sam Elliott <soe1@uakron.edu>
2023-09-20 18:46:56 -0400

    working on actually getting the React together: starting with the background color and the text above the frog!

---
+++ frog-chores/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import chores from './choresdone.png'
+
+function App() {
+  return (
+    <div className="App">
+
+      <img src={chores} alt='choresdone' />
+
+    </div>
+  );
+}
+
+export default App;
