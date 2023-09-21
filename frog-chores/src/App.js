d129a758b99218a9249ee32b36196686eb12181c
Sam Elliott <soe1@uakron.edu>
2023-09-21 11:59:49 -0400

    alright, we are getting state to work and the button to transition images!

--- frog-chores/src/App.js
+++ frog-chores/src/App.js
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import chores from './choresdone.png'
 import expectantFrog from './firstfrog.png'
 import buttonOne from './buttonfirst.png'
+import buttonTwo from './buttonsecond.png'
 
 function App() {
+
+  const [btnImg, setBtn] = useState(buttonOne);
+
+  function handleClick() {
+    console.log('clicked!');
+    setBtn(buttonTwo);
+  }
+
   return (
     <div className="App">
       <img src={chores} alt='choresdone' />
       <img src={expectantFrog} alt='frog1' />
-      <button><img src={buttonOne} alt='choresdone' />  </button>
+      <button onClick={handleClick}><img src={btnImg} alt='choresdone' />  </button>
     </div>
   );
 }
