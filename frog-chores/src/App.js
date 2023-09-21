83bd6a19a7a2b903650430a121613ac60c868429
Sam Elliott <soe1@uakron.edu>
2023-09-21 11:39:00 -0400

    alright, got the border around the button gone, now we need to get the animation working!

--- frog-chores/src/App.js
+++ frog-chores/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import chores from './choresdone.png'
 import expectantFrog from './firstfrog.png'
+import buttonOne from './buttonfirst.png'
 
 function App() {
   return (
     <div className="App">
       <img src={chores} alt='choresdone' />
       <img src={expectantFrog} alt='frog1' />
+      <button><img src={buttonOne} alt='choresdone' />  </button>
     </div>
   );
 }
