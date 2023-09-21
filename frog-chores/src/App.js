5c8c0b8e3965dae4bd475c2cc7942c7aa2c267ab
Sam Elliott <soe1@uakron.edu>
2023-09-21 18:52:50 -0400

    alrighty, so almost all of the animation works, just need to go back to the first image after the click

--- frog-chores/src/App.js
+++ frog-chores/src/App.js
@@ -4,21 +4,43 @@ import chores from './choresdone.png'
 import expectantFrog from './firstfrog.png'
 import buttonOne from './buttonfirst.png'
 import buttonTwo from './buttonsecond.png'
+import buttonThree from './buttonthird.png'
 
 function App() {
 
-  const [btnImg, setBtn] = useState(buttonOne);
+  const [state, setState] = useState({
+    showFirst: true,
+    showSecond: false,
+    showThird: false
+  });
 
   function handleClick() {
-    console.log('clicked!');
-    setBtn(buttonTwo);
+    setState({
+      showFirst: false,
+      showSecond: true,
+    });
+
+    setState({
+      showSecond: false,
+      showThird: true,
+    });
+
+
   }
+
+
+  const { showFirst, showSecond, showThird } = state;
 
   return (
     <div className="App">
       <img src={chores} alt='choresdone' />
       <img src={expectantFrog} alt='frog1' />
-      <button onClick={handleClick}><img src={btnImg} alt='choresdone' />  </button>
+
+      <button onClick={handleClick}>
+        <img id="img1" className={`${showFirst ? "visible" : "hidden"}`} src={buttonOne} alt='choresdone' />
+        <img id="img2" className={`${showSecond ? "visible" : "hidden"}`} src={buttonTwo} alt='choresdone' />
+        <img id="img3" className={`${showThird ? "visible" : "hidden"}`} src={buttonThree} alt='choresdone' />
+      </button>
     </div>
   );
 }
